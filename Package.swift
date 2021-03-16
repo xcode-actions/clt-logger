3f5a5316ed7e65825f2b0d80fe4a5bb83e6e6da2
Frizlab <fload@me.com>
2021-03-16 23:02:36 +0200

    First full implementation of the logger

--- Package.swift
+++ Package.swift
@@ -4,15 +4,20 @@ import PackageDescription
 
 let package = Package(
 	name: "CLTLogger",
+	platforms: [
+		.macOS(.v10_12)
+	],
 	products: [
 		.library(name: "CLTLogger", targets: ["CLTLogger"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2")
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
+		.package(url: "https://github.com/apple/swift-system.git", .branch("main")) /* Sadly, standardOutput and standardError are not available in 0.0.1 */
 	],
 	targets: [
 		.target(name: "CLTLogger", dependencies: [
-			.product(name: "Logging", package: "swift-log")
+			.product(name: "Logging",       package: "swift-log"),
+			.product(name: "SystemPackage", package: "swift-system")
 		]),
 		.testTarget(name: "CLTLoggerTests", dependencies: ["CLTLogger"])
 	]
--- Sources/CLTLogger/CLTLogger.swift
+++ Sources/CLTLogger/CLTLogger.swift
@@ -1,21 +1,71 @@
 import Foundation
 
 import Logging
+import SystemPackage
 
 
 
-struct CLTLogger : LogHandler {
+/**
+A logger designed for Command Line Tools.
+
+A few things:
+- Output is UTF8. Always.
+- If the write syscall fails, the log is lost (except interrupts which are
+retried; see SystemPackage for more info).
+- Default CLTLogger (no-args init) will log everything below warning (excluded)
+to stdout, and everything above warning (included) to stderr.
+- The logger uses an os\_unfair\_lock which might not be available outside macOS
+unfortunately (untested). */
+public struct CLTLogger : LogHandler {
 	
-	var logLevel: Logger.Level = .info
-	var metadata: Logger.Metadata = [:]
+	public var logLevel: Logger.Level = .info
+	public var metadata: Logger.Metadata = [:]
 	
-	subscript(metadataKey metadataKey: String) -> Logger.Metadata.Value? {
+	public let fdMap: [Logger.Level: FileDescriptor]
+	
+	public init() {
+		self.init(fdChanges: [
+			.trace:    .standardOutput,
+			/* Below warning, logs to stdout */
+			.warning:  .standardError,
+			/* Above and including warning, logs to stderr */
+		])
+	}
+	
+	public init(fdChanges: [Logger.Level: FileDescriptor?]) {
+		var fdMap = [Logger.Level: FileDescriptor]()
+		
+		var currentFileDescriptor: FileDescriptor?
+		for l in Logger.Level.allCases {
+			if let newFd = fdChanges[l] {
+				currentFileDescriptor = newFd
+			}
+			if let fd = currentFileDescriptor {
+				fdMap[l] = fd
+			}
+		}
+		self.init(fdMap: fdMap)
+	}
+	
+	public init(fdMap: [Logger.Level: FileDescriptor]) {
+		self.fdMap = fdMap
+	}
+	
+	public subscript(metadataKey metadataKey: String) -> Logger.Metadata.Value? {
 		get {metadata[metadataKey]}
 		set {metadata[metadataKey] = newValue}
 	}
 	
-	func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, source: String, file: String, function: String, line: UInt) {
+	public func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, source: String, file: String, function: String, line: UInt) {
+		guard let fd = fdMap[level] else {return}
 		
+		/* flockfile is only available for FILE operations, not fd. */
+		os_unfair_lock_lock(&CLTLogger.lock)
+		/* Is there a better idea than silently drop the message in case of fail? */
+		_ = try? fd.writeAll(Data(message.description.utf8))
+		os_unfair_lock_unlock(&CLTLogger.lock)
 	}
+	
+	private static var lock = os_unfair_lock_s()
 	
 }
