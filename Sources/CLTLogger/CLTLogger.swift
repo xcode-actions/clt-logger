05b571450db98ddcfe4f13bab7ee0c0772b5df68
Frizlab <fload@me.com>
2021-03-16 20:26:10 +0200

    Implement empty shell for the logger, w/ test from doc

--- Sources/CLTLogger/CLTLogger.swift
+++ Sources/CLTLogger/CLTLogger.swift
@@ -1,3 +1,21 @@
-struct CLTLogger {
-	var text = "Hello, World!"
+import Foundation
+
+import Logging
+
+
+
+struct CLTLogger : LogHandler {
+	
+	var logLevel: Logger.Level = .info
+	var metadata: Logger.Metadata = [:]
+	
+	subscript(metadataKey metadataKey: String) -> Logger.Metadata.Value? {
+		get {metadata[metadataKey]}
+		set {metadata[metadataKey] = newValue}
+	}
+	
+	func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, source: String, file: String, function: String, line: UInt) {
+		
+	}
+	
 }
