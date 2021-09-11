10c3f8e57b10be2594f4d104dfb6a1eff23ea4da
Frizlab <fload@me.com>
2021-09-11 17:00:24 +0300

    Update swift-system dependency version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-		.package(url: "https://github.com/apple/swift-system.git", from: "0.0.2")
+		.package(url: "https://github.com/apple/swift-system.git", from: "1.0.0")
 	],
 	targets: [
 		.target(name: "CLTLogger", dependencies: [
